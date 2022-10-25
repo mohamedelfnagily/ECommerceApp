8b8bb60eb626fda239b947fa52d9052bf2c719e5
mohamedelfnagily <m.elfnagily96@gmail.com>
2022-10-25 22:18:17 +0200

    Adding Authentication controller and implementing methods

--- ECommerceApp.API/Controllers/AuthController.cs
+++ ECommerceApp.API/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerceApp.BL.DTOs.AuthDTOs;
+using ECommerceApp.BL.Managers.AuthManager;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceApp.API.Controllers
@@ -7,5 +9,35 @@ namespace ECommerceApp.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        //this controller is responsible for loging in and regestring new users
+        private readonly IAuthenticationManager _manager;
+        public AuthController(IAuthenticationManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public ActionResult Register(RegisterDto model)
+        {
+            var userData = _manager.RegisterNewUSer(model);
+            if(userData==null)
+            {
+                return BadRequest();
+            }
+            return Ok(userData);
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public ActionResult Login(LoginDto model)
+        {
+            var userData = _manager.LoginUser(model);
+            if (userData == null)
+            {
+                return BadRequest();
+            }
+            return Ok(userData);
+        }
     }
 }
